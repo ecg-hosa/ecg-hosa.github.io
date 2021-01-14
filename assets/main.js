b3b71b4870d86f26f0679260fd9c636cac8ae736
Taylor Yang <tayloryang.0721@gmail.com>
2021-01-13 19:15:26 -0500

    added 404 error page

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,11 @@ function openNav() {
 function closeNav() {
 	document.getElementById("menu").style.width = "0%";
 }
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+	var search = form.querySelector("input[type=search]");
+	search.value = "site:ecg-hosa.github.io" + search.value;
+	form.submit();
+});
